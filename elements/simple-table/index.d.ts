8240f927c127c1aad6ae1346bf09bf7c3fd322a5
Joshua Godi <jgodi@bullhorn.com>
2017-12-01 03:00:53 +0000

    072de28 fix

---
+++ elements/simple-table/index.d.ts
@@ -0,0 +1,11 @@
+export * from './cell';
+export * from './table';
+export * from './row';
+export * from './cell-header';
+export * from './sort';
+export * from './interfaces';
+export * from './table-source';
+export * from './activity-table-renderers';
+export * from './state';
+export declare class NovoSimpleTableModule {
+}
---
+++ novo-elements.providers.d.ts
@@ -0,0 +1,43 @@
+import { GooglePlacesService } from './elements/places/places.service';
+import { NovoDragulaService } from './elements/dragula/DragulaService';
+import { NovoModalService } from './elements/modal/ModalService';
+import { NovoModalRef } from './elements/modal/Modal';
+import { NovoToastService } from './elements/toast/ToastService';
+import { ComponentUtils } from './utils/component-utils/ComponentUtils';
+import { FieldInteractionApi } from './elements/form/FieldInteractionApi';
+import { DateFormatService } from './services/date-format/DateFormat';
+import { GlobalRef, BrowserGlobalRef } from './services/global/global.service';
+import { LocalStorageService } from './services/storage/storage.service';
+export declare class NovoElementProviders {
+    static forRoot(): {
+        ngModule: typeof NovoElementProviders;
+        providers: (typeof DateFormatService | typeof FieldInteractionApi | {
+            provide: typeof NovoDragulaService;
+            useClass: typeof NovoDragulaService;
+        } | {
+            provide: typeof NovoModalRef;
+            useClass: typeof NovoModalRef;
+        } | {
+            provide: typeof NovoModalService;
+            useClass: typeof NovoModalService;
+        } | {
+            provide: typeof GooglePlacesService;
+            useClass: typeof GooglePlacesService;
+        } | {
+            provide: typeof NovoToastService;
+            useClass: typeof NovoToastService;
+        } | {
+            provide: typeof ComponentUtils;
+            useClass: typeof ComponentUtils;
+        } | {
+            provide: typeof GlobalRef;
+            useClass: typeof BrowserGlobalRef;
+        } | {
+            provide: typeof LocalStorageService;
+            useClass: typeof LocalStorageService;
+        })[];
+    };
+    static forChild(): {
+        ngModule: typeof NovoElementProviders;
+    };
+}
