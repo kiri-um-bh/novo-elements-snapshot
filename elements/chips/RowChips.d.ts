9b672348e17ec5ad014193be344143b1bbf6bcad
Willie Hinton <william.hinton@toolbox9.com>
2018-08-28 17:13:50 +0000

    f8a8d0c Hide labels when empty.

---
+++ elements/chips/RowChips.d.ts
@@ -0,0 +1,11 @@
+import { ElementRef } from '@angular/core';
+import { NovoLabelService } from '../../services/novo-label-service';
+import { ComponentUtils } from '../../utils/component-utils/ComponentUtils';
+import { NovoChipElement, NovoChipsElement } from './Chips';
+export declare class NovoRowChipElement extends NovoChipElement {
+    onSelect(e: any): boolean;
+}
+export declare class NovoRowChipsElement extends NovoChipsElement {
+    constructor(element: ElementRef, componentUtils: ComponentUtils, labels: NovoLabelService);
+    onKeyDown(event: any): void;
+}
