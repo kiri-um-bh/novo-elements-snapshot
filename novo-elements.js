b5d0ce0c85d963f2dcaacadbaac53f43c12f4214
Adam Crowe <acrowe@bullhorn.com>
2018-08-29 14:33:45 +0000

    24ba626 add calibri font to ckeditor

--- novo-elements.js
+++ novo-elements.js
@@ -14041,6 +14041,16 @@ class NovoCKEditorElement {
             disableNativeSpellChecker: false,
             removePlugins: 'liststyle,tabletools,contextmenu',
             extraAllowedContent: '*(*){*};table tbody tr td th[*];',
+            font_names: 'Arial/Arial, Helvetica, sans-serif;' +
+                'Calibri/Calibri, Verdana, Geneva, sans-serif;' +
+                'Comic Sans MS/Comic Sans MS, cursive;' +
+                'Courier New/Courier New, Courier, monospace;' +
+                'Georgia/Georgia, serif;' +
+                'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+                'Tahoma/Tahoma, Geneva, sans-serif;' +
+                'Times New Roman/Times New Roman, Times, serif;' +
+                'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+                'Verdana/Verdana, Geneva, sans-serif',
         };
         const /** @type {?} */ minimalConfig = {
             toolbar: [
