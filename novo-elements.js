896814413b04df9ca465dfe3efeca007dde9d9b9
Nicholas Lewis <nlewis@bullhorn.com>
2018-09-26 20:56:45 +0000

    15e6638 Fix issue with novo tooltip styling

--- novo-elements.js
+++ novo-elements.js
@@ -1341,7 +1341,7 @@ NovoTooltip.decorators = [
                 template: `<div [@state]="noAnimate ? 'no-animation' : 'visible'" 
   [ngClass]="[tooltipType, this.rounded ? 'rounded' : '', size ? size : '', this.preline? 'preline' : '', position]">{{message}}</div>`,
                 styles: [`
-    :host div {
+    novo-tooltip div {
       background: #383838;
       color: #fff;
       padding: 8px 10px;
@@ -1352,146 +1352,146 @@ NovoTooltip.decorators = [
       text-shadow: 0 -1px 0 #000;
       -webkit-box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3);
               box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3); }
-      :host div.error {
+      novo-tooltip div.error {
         background-color: #b34e4d;
         text-shadow: 0 -1px 0 #592726; }
-      :host div.info {
+      novo-tooltip div.info {
         background-color: #3986ac;
         text-shadow: 0 -1px 0 #1a3c4d; }
-      :host div.warning {
+      novo-tooltip div.warning {
         background-color: #c09854;
         text-shadow: 0 -1px 0 #6c5328; }
-      :host div.success {
+      novo-tooltip div.success {
         background-color: #458746;
         text-shadow: 0 -1px 0 #1a321a; }
-      :host div.rounded {
+      novo-tooltip div.rounded {
         border-radius: 4px; }
-      :host div.extra-large, :host div.large, :host div.small, :host div.medium {
+      novo-tooltip div.extra-large, novo-tooltip div.large, novo-tooltip div.small, novo-tooltip div.medium {
         white-space: normal;
         line-height: 1.4em;
         word-wrap: break-word; }
-      :host div.extra-large {
+      novo-tooltip div.extra-large {
         width: 400px;
         font-size: 1.2vh; }
-      :host div.large {
+      novo-tooltip div.large {
         width: 300px; }
-      :host div.medium {
+      novo-tooltip div.medium {
         width: 150px; }
-      :host div.small {
+      novo-tooltip div.small {
         width: 80px; }
-      :host div.preline {
+      novo-tooltip div.preline {
         white-space: pre-line; }
-      :host div.top:before {
+      novo-tooltip div.top:before {
         margin-bottom: -11px;
         left: calc(50% - 6px);
         bottom: 0;
         border-top-color: #383838; }
-      :host div.top.error:before {
+      novo-tooltip div.top.error:before {
         border-top-color: #b34e4d; }
-      :host div.top.info:before {
+      novo-tooltip div.top.info:before {
         border-top-color: #3986ac; }
-      :host div.top.warning:before {
+      novo-tooltip div.top.warning:before {
         border-top-color: #c09854; }
-      :host div.top.success:before {
+      novo-tooltip div.top.success:before {
         border-top-color: #458746; }
-      :host div.top-left:before {
+      novo-tooltip div.top-left:before {
         border-top-color: #383838;
         margin-right: 0;
         margin-bottom: -11px;
         right: 0;
         bottom: 0; }
-      :host div.top-left.error:before {
+      novo-tooltip div.top-left.error:before {
         border-top-color: #b34e4d; }
-      :host div.top-left.info:before {
+      novo-tooltip div.top-left.info:before {
         border-top-color: #3986ac; }
-      :host div.top-left.warning:before {
+      novo-tooltip div.top-left.warning:before {
         border-top-color: #c09854; }
-      :host div.top-left.success:before {
+      novo-tooltip div.top-left.success:before {
         border-top-color: #458746; }
-      :host div.top-right:before {
+      novo-tooltip div.top-right:before {
         border-top-color: #383838;
         margin-left: 0;
         margin-bottom: -11px;
         left: 0;
         bottom: 0; }
-      :host div.top-right.error:before {
+      novo-tooltip div.top-right.error:before {
         border-top-color: #b34e4d; }
-      :host div.top-right.info:before {
+      novo-tooltip div.top-right.info:before {
         border-top-color: #3986ac; }
-      :host div.top-right.warning:before {
+      novo-tooltip div.top-right.warning:before {
         border-top-color: #c09854; }
-      :host div.top-right.success:before {
+      novo-tooltip div.top-right.success:before {
         border-top-color: #458746; }
-      :host div.bottom:before {
+      novo-tooltip div.bottom:before {
         margin-top: -11px;
         left: calc(50% - 6px);
         top: 0;
         border-bottom-color: #383838; }
-      :host div.bottom.error:before {
+      novo-tooltip div.bottom.error:before {
         border-top-color: #b34e4d; }
-      :host div.bottom.info:before {
+      novo-tooltip div.bottom.info:before {
         border-top-color: #3986ac; }
-      :host div.bottom.warning:before {
+      novo-tooltip div.bottom.warning:before {
         border-top-color: #c09854; }
-      :host div.bottom.success:before {
+      novo-tooltip div.bottom.success:before {
         border-top-color: #458746; }
-      :host div.bottom-left:before {
+      novo-tooltip div.bottom-left:before {
         border-bottom-color: #383838;
         margin-right: 0;
         margin-top: -11px;
         right: 0;
         top: 0; }
-      :host div.bottom-left.error:before {
+      novo-tooltip div.bottom-left.error:before {
         border-bottom-color: #b34e4d; }
-      :host div.bottom-left.info:before {
+      novo-tooltip div.bottom-left.info:before {
         border-bottom-color: #3986ac; }
-      :host div.bottom-left.warning:before {
+      novo-tooltip div.bottom-left.warning:before {
         border-bottom-color: #c09854; }
-      :host div.bottom-left.success:before {
+      novo-tooltip div.bottom-left.success:before {
         border-bottom-color: #458746; }
-      :host div.bottom-right:before {
+      novo-tooltip div.bottom-right:before {
         border-bottom-color: #383838;
         margin-left: 0;
         margin-top: -11px;
         left: 0;
         top: 0; }
-      :host div.bottom-right.error:before {
+      novo-tooltip div.bottom-right.error:before {
         border-bottom-color: #b34e4d; }
-      :host div.bottom-right.info:before {
+      novo-tooltip div.bottom-right.info:before {
         border-bottom-color: #3986ac; }
-      :host div.bottom-right.warning:before {
+      novo-tooltip div.bottom-right.warning:before {
         border-bottom-color: #c09854; }
-      :host div.bottom-right.success:before {
+      novo-tooltip div.bottom-right.success:before {
         border-bottom-color: #458746; }
-      :host div.left:before {
+      novo-tooltip div.left:before {
         border-left-color: #383838;
         margin-right: -11px;
         margin-bottom: -6px;
         right: 0;
         bottom: 50%; }
-      :host div.left.error:before {
+      novo-tooltip div.left.error:before {
         border-left-color: #b34e4d; }
-      :host div.left.info:before {
+      novo-tooltip div.left.info:before {
         border-left-color: #3986ac; }
-      :host div.left.warning:before {
+      novo-tooltip div.left.warning:before {
         border-left-color: #c09854; }
-      :host div.left.success:before {
+      novo-tooltip div.left.success:before {
         border-left-color: #458746; }
-      :host div.right:before {
+      novo-tooltip div.right:before {
         left: 0;
         bottom: 50%;
         border-right-color: #383838;
         margin-left: -11px;
         margin-bottom: -6px; }
-      :host div.right.error:before {
+      novo-tooltip div.right.error:before {
         border-right-color: #b34e4d; }
-      :host div.right.info:before {
+      novo-tooltip div.right.info:before {
         border-right-color: #3986ac; }
-      :host div.right.warning:before {
+      novo-tooltip div.right.warning:before {
         border-right-color: #c09854; }
-      :host div.right.success:before {
+      novo-tooltip div.right.success:before {
         border-right-color: #458746; }
-      :host div:before {
+      novo-tooltip div:before {
         content: '';
         position: absolute;
         background: 0 0;
