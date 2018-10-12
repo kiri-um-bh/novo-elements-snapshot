fe75bc38fb4cf76eccea099c00bc5298de0faaea
Jay Quach <>
2018-10-12 18:32:03 +0000

    e3f801c Added back missing code

--- novo-elements.js
+++ novo-elements.js
@@ -40391,8 +40391,11 @@ NovoValueElement.decorators = [
         <ng-container [ngSwitch]="type">
             <div class="value-outer" *ngIf="showLabel">
                 <label>{{ meta.label }}</label>
-                <a *ngSwitchCase="NOVO_VALUE_TYPE.INTERNAL_LINK" class="value" (click)="openLink()" [innerHTML]="data | render : meta"></a>
-                <a *ngSwitchCase="NOVO_VALUE_TYPE.LINK" class="value" [href]="url" target="_blank" [innerHTML]="data | render : meta"></a>
+                <span class="value">
+                  <i *ngIf="meta.showEntityIcon" class="bhi-circle {{meta.entityIconClass}}"></i>
+                  <a *ngSwitchCase="NOVO_VALUE_TYPE.INTERNAL_LINK" (click)="openLink()" [innerHTML]="data | render : meta"></a>
+                  <a *ngSwitchCase="NOVO_VALUE_TYPE.LINK" class="value" [href]="url" target="_blank" [innerHTML]="data | render : meta"></a>
+                </span>
                 <novo-entity-list *ngSwitchCase="NOVO_VALUE_TYPE.ENTITY_LIST" [data]='data' [meta]="meta"></novo-entity-list>
             </div>
             <div *ngSwitchDefault class="value-outer" [ngClass]="customClass">
