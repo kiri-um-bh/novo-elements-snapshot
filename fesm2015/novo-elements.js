0e04d82f10b3cba4823cc23675674d42c17d1ae9
Christopher Erxleben <cerxleben.fhs@gmail.com>
2019-02-27 16:33:43 +0000

    0e9b7ae bug(helpers): Fixed issue where isNumber was validating incorrectly (#920)

--- fesm2015/novo-elements.js
+++ fesm2015/novo-elements.js
@@ -140,7 +140,12 @@ class Helpers {
      * @return {?}
      */
     static isNumber(val) {
-        return !isNaN(parseFloat(val));
+        if (typeof val === 'string') {
+            return val.length > 0 && val !== '.' && /^\d*\.?\d*$/.test(val);
+        }
+        else {
+            return !isNaN(parseFloat(val));
+        }
     }
     /**
      * Checks to see if the object is a undefined or null
