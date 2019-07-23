9761718cb6c85586d26a06c3ab3c67326ac09af8
chasitywickenhauser-bh <46939241+chasitywickenhauser-bh@users.noreply.github.com>
2019-07-23 20:15:20 +0000

    8bef27e feat(data-table): added flag to right align table cell data in column (#1011)

--- bundles/novo-elements.umd.js
+++ bundles/novo-elements.umd.js
@@ -48796,6 +48796,9 @@
                 if (this.column.cellClass) {
                     this.renderer.addClass(this.elementRef.nativeElement, this.column.cellClass(this.row));
                 }
+                if (this.column.rightAlignCellContent) {
+                    this.renderer.addClass(this.elementRef.nativeElement, 'novo-data-table-cell-align-right');
+                }
                 this.calculateWidths();
                 this.subscriptions.push(this.resized.subscribe(function (column) {
                     if (column === _this.column) {
