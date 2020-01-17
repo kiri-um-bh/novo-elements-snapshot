32780b71453c529a4da061bc736ff1f53bfa288b
Adrian Ossi <adrianossi@users.noreply.github.com>
2020-01-17 17:12:00 +0000

    1fd7713 Merge branch 'master' into f/fixStickyTableFilters

--- bundles/novo-elements.umd.js
+++ bundles/novo-elements.umd.js
@@ -54176,6 +54176,9 @@
                     _this.totalSet = true;
                     _this.state.isForceRefresh = false;
                 }
+                else if (data.total > _this.total) {
+                    _this.total = data.total;
+                }
                 _this.currentTotal = data.total;
                 _this.current = data.results.length;
                 _this.data = data.results;
